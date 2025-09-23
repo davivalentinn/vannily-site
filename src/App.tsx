78091517214d844004056b1b1529378562dd51bf
davivalentinn <davivalentns@gmail.com>
2025-09-23 08:12:55 -0500

    Ajustando merge inicial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind funcionando! 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
